b2219b1c7a5a5e0f999a7fac85f772b580e3bb75
Andy <andy.dlg.lp@gmail.com>
2019-05-15 09:08:03 -0700

    final test build

---
+++ src/posts.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { List, Datagrid, TextField, ReferenceField, EditButton, Create, Edit } from 'react-admin';
+import { Filter, ReferenceInput, SelectInput, TextInput, LongTextInput, SimpleForm, DisabledInput } from 'react-admin';
+
+export const PostCreate = props => (
+    <Create {...props}>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <LongTextInput source="body" />
+        </SimpleForm>
+    </Create>
+);
+
+const PostTitle = ({ record }) => {
+    return <span>Post {record ? `"${record.title}"` : ''}</span>;
+};
+
+export const PostEdit = props => (
+   <Edit title={<PostTitle />} {...props}>
+        <SimpleForm>
+           <DisabledInput source="id" />
+            <ReferenceInput source="userId" reference="users">
+               <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <LongTextInput source="body" />
+        </SimpleForm>
+    </Edit>
+);
+
+const PostFilter = (props) => (
+    <Filter {...props}>
+        <TextInput label="Search" source="q" alwaysOn />
+        <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+            <SelectInput optionText="name" />
+        </ReferenceInput>
+    </Filter>
+);
+
+export const PostList = (props) => (
+    <List filters={<PostFilter />} {...props}>
+        <Datagrid>
+        <TextField source="id" />
+            <ReferenceField source="userId" reference="users">
+                <TextField source="name" />
+            </ReferenceField>
+            <TextField source="id" />
+            <TextField source="title" />
+            <TextField source="body" />
+            <EditButton />
+        </Datagrid>
+    </List>
+);
